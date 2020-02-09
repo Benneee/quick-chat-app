1026a739adb2bb8029a48f135cba7d730a1efcae
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-02-09 11:58:53 +0100

    Precompleted quick-chat-app

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,12 @@ const log = console.log;
 
 const form = document.querySelector("#chatForm");
 const ul = document.querySelector("#messages");
+const annUl = document.querySelector("#announcement");
+const welcomeTextTag = document.querySelector("#welcomeText");
+
+socket.on("new user", welcomeText => {
+  welcomeTextTag.textContent = welcomeText;
+});
 
 form.addEventListener("submit", e => {
   const input = document.querySelector("#m");
@@ -21,4 +27,13 @@ socket.on("chat message", msg => {
   list.appendChild(text);
 
   ul.appendChild(list);
+});
+
+socket.on("announcement", announcedInfo => {
+  // Receiving the announcement and displaying on the screen
+  const list = document.createElement("li");
+  const text = document.createTextNode(announcedInfo);
+  list.appendChild(text);
+
+  annUl.appendChild(list);
 });
